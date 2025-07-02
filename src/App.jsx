2b2440520e684217c90a731edb5139f69a373579
Harsh Shrivastava <harshsrivastava0302@gmail.com>
2025-07-02 15:08:05 +0530

    supabase setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {Button} from "./components/ui/button";
+import React from "react"
+function App() {
+
+  return (
+    <>
+      <div className="text-3xl font-bold underline">Harsh</div>
+      <Button variant="outline" className="cursor-pointer">Hi</Button>
+    </>
+  )
+}
+
+export default App
