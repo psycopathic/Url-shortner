d0ea814c94f3d389462637670e687fe6ae615aa1
Harsh Shrivastava <harshsrivastava0302@gmail.com>
2025-07-03 19:12:36 +0530

    this commit include the working of login and context api logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,49 @@
-import {Button} from "./components/ui/button";
-import React from "react"
-import { createBrowserRouter,RouterProvider } from "react-router-dom";
+import { Button } from "./components/ui/button";
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import AppLayout from "./layout/App-layout";
 import LandingPage from "./pages/Landing";
 import Dashboard from "./pages/Dashboard";
 import Link from "./pages/Link";
 import Auth from "./pages/Auth";
 import RedirectLink from "./pages/RedirectLink";
-
-
-
-
-
+import UrlProvider from "./Context";
 
 const router = createBrowserRouter([
   {
-    element:<AppLayout/>,
-    children:[
+    element: <AppLayout />,
+    children: [
       {
-        path:'/',
-        element:<LandingPage/>
+        path: "/",
+        element: <LandingPage />,
       },
       {
-        path:'/dashboard',
-        element:<Dashboard/>
+        path: "/dashboard",
+        element: <Dashboard />,
       },
       {
-        path:'/auth',
-        element:<Auth/>
+        path: "/auth",
+        element: <Auth />,
       },
       {
-        path:'/link/:id',
-        element:<Link/>
+        path: "/link/:id",
+        element: <Link />,
       },
       {
-        path:'/:id',
-        element:<RedirectLink/>
+        path: "/:id",
+        element: <RedirectLink />,
       },
-    ]
-  }
-])
+    ],
+  },
+]);
 function App() {
-
   return (
     <>
-      <RouterProvider router={router}/>
+      <UrlProvider>
+        <RouterProvider router={router} />
+      </UrlProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/db/apiAuth.js
@@ -0,0 +1,27 @@
+//here we will be writing the login and signup logic for the application
+
+import supabase from "./supabase.js";
+
+export async function login({email,password}){
+   const {data,error} = await supabase.auth.signInWithPassword({
+    email,
+    password
+   });
+   if(error)throw new Error(error.message);
+   return data;
+}
+
+export async function getCurrentUser() {
+  const {data: session, error} = await supabase.auth.getSession();
+  if (!session.session) return null;
+
+  // const {data, error} = await supabase.auth.getUser();
+
+  if (error) throw new Error(error.message);
+  return session.session?.user;
+}
+
+export async function logout() {
+  const {error} = await supabase.auth.signOut();
+  if (error) throw new Error(error.message);
+}
